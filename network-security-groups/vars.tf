13e7634550c7eb84229b5883a82c3d5bfaffed2e
Ryan Powers <ryan.p.powers@gmail.com>
2021-05-12 20:33:35 +0000

    Adding network security groups demo project

---
+++ network-security-groups/vars.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  type    = string
+  default = "southcentralus"
+}
+
+variable "prefix" {
+  type    = string
+  default = "demo"
+}
+
+variable "ssh-source-address" {
+  type    = string
+  default = "*"
+}
