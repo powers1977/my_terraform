176bcebcc4688c9ccb668fbf622cb18026c4fbf3
Ryan Powers <ryan.p.powers@gmail.com>
2021-05-12 23:33:38 +0000

    Ran terraform fmt command

--- network-security-groups/instance.tf
+++ network-security-groups/instance.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_machine" "demo-instance-1" {
   vm_size               = "Standard_A1_v2"
 
   # this is a demo instance, so we can delete all data on termination
-  delete_os_disk_on_termination = true
+  delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
 
   storage_image_reference {
@@ -36,18 +36,18 @@ resource "azurerm_virtual_machine" "demo-instance-1" {
 }
 
 resource "azurerm_network_interface" "demo-instance-1" {
-  name                      = "${var.prefix}-instance1"
-  location                  = var.location
-  resource_group_name       = azurerm_resource_group.demo.name
+  name                = "${var.prefix}-instance1"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.demo.name
   # from terraform-provider-azurerm >2.0 we need to put this in a seperate resource
   # see azurerm_network_interface_security_group_association below
   # network_security_group_id = azurerm_network_security_group.allow-ssh.id
 
   ip_configuration {
-    name                           = "instance1"
-    subnet_id                      = azurerm_subnet.demo-internal-1.id
-    private_ip_address_allocation  = "Dynamic"
-    public_ip_address_id           = azurerm_public_ip.demo-instance-1.id
+    name                          = "instance1"
+    subnet_id                     = azurerm_subnet.demo-internal-1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.demo-instance-1.id
   }
 }
 
@@ -58,10 +58,10 @@ resource "azurerm_network_interface_security_group_association" "demo-instance-1
 
 
 resource "azurerm_public_ip" "demo-instance-1" {
-    name                         = "instance1-public-ip"
-    location                     = var.location
-    resource_group_name          = azurerm_resource_group.demo.name
-    allocation_method            = "Dynamic"
+  name                = "instance1-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.demo.name
+  allocation_method   = "Dynamic"
 }
 
 resource "azurerm_application_security_group" "demo-instance-group" {
@@ -84,7 +84,7 @@ resource "azurerm_virtual_machine" "demo-instance-2" {
   vm_size               = "Standard_A1_v2"
 
   # this is a demo instance, so we can delete all data on termination
-  delete_os_disk_on_termination = true
+  delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
 
   storage_image_reference {
@@ -113,17 +113,17 @@ resource "azurerm_virtual_machine" "demo-instance-2" {
 }
 
 resource "azurerm_network_interface" "demo-instance-2" {
-  name                      = "${var.prefix}-instance2"
-  location                  = var.location
-  resource_group_name       = azurerm_resource_group.demo.name
+  name                = "${var.prefix}-instance2"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.demo.name
   # from terraform-provider-azurerm >2.0 we need to put this in a seperate resource
   # see azurerm_network_interface_security_group_association below
   # network_security_group_id = azurerm_network_security_group.internal-facing.id
 
   ip_configuration {
-    name                           = "instance2"
-    subnet_id                      = azurerm_subnet.demo-internal-1.id
-    private_ip_address_allocation  = "Dynamic"
+    name                          = "instance2"
+    subnet_id                     = azurerm_subnet.demo-internal-1.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
